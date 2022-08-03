58d91e304f72d79a41d1c950d26bf15f8eea5e22
Scott Andrew <scott@nwdm.tech>
2022-08-02 20:23:10 -0700

    Initial Commit

---
+++ HousingData/HousingDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HousingDataApp.swift
+//  HousingData
+//
+//  Created by Scott Andrew on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct HousingDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
